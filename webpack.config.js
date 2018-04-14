4fb1ec2494b12c7baf4f2eb4df1f2f3a51d2f77b
MinhongShen <761591766@qq.com>
2018-04-14 18:08:06 +0800

    使用CommonChunkPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,29 @@
 /*
 * @Author: 761591766@qq.com
 * @Date:   2018-04-14 00:49:22
-* @Last Modified by:   761591766@qq.com
-* @Last Modified time: 2018-04-14 01:24:11
+* @Last Modified by:   Minhong Shen
+* @Last Modified time: 2018-04-14 16:59:44
 */
+var webpack = require('webpack');
+
 var config = {
     entry: {
         'index': ['./src/page/index/index.js'],
         'login': ['./src/page/login/login.js']
     },
     output: {
-        path: './dist',
+        path: __dirname + '/dist',
         filename: 'js/[name].js'
     },
     externals: {
         'jquery': 'window.jQuery'
-    }
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'commons',
+            filename: 'js/base.js'
+        })
+    ]
 };
 
 module.exports = config;
